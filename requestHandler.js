b6352cf77d8c316fbb63a1be6e7cccb109c42bdd
junho2408 <bagho1999@gmail.com>
2024-08-23 22:56:49 +0900

    1.0.0

--- requestHandler.js
+++ requestHandler.js
@@ -1,32 +1,51 @@
+const fs = require('fs'); // FS : File Sink, html 파일을 가져오는 모듈
+const main_view = fs.readFileSync('./main.html', 'utf-8');
+
 const mariadb = require('./database/connect/mariadb');
 
 function main(response){
-    console.log('main');
-
     mariadb.query("select * from product", function(err, rows){
         console.log(rows);
     });
-
     response.writeHead(200, {'Content-Type' : 'text/html'}); //응답의 헤드 부분(통신 상태, 응답의 유형)
-    response.write('Main page'); //응답의 바디 부분(표시할 내용)
+    response.write(main_view); //응답의 바디 부분(표시할 내용)
     response.end();
 }
 
-function login(response){
-    console.log('login');
-
-    response.writeHead(200, {'Content-Type' : 'text/html'}); //응답의 헤드 부분(통신 상태, 응답의 유형)
-    response.write('Login page'); //응답의 바디 부분(표시할 내용)
-    response.end();
+function diablo4(response){
+    fs.readFile('./img/diablo_4.jpg', function(err,data){
+        response.writeHead(200, {'Content-Type' : 'text/html'}); //응답의 헤드 부분(통신 상태, 응답의 유형)
+        response.write(data); //응답의 바디 부분(표시할 내용)
+        response.end();
+    });    
 }
 
-// function junho(response){
-//     console.log('junho');
+function eldenRing(response){
+    fs.readFile('./img/elden_ring.jpg', function(err,data){
+        response.writeHead(200, {'Content-Type' : 'text/html'}); //응답의 헤드 부분(통신 상태, 응답의 유형)
+        response.write(data); //응답의 바디 부분(표시할 내용)
+        response.end();
+    });    
+}
 
-//     response.writeHead(200, {'Content-Type' : 'text/html'}); //응답의 헤드 부분(통신 상태, 응답의 유형)
-//     response.write('Kim Junho'); //응답의 바디 부분(표시할 내용)
-//     response.end();
-// }
+function onceHuman(response){
+    fs.readFile('./img/once_human.jpg', function(err,data){
+        response.writeHead(200, {'Content-Type' : 'text/html'}); //응답의 헤드 부분(통신 상태, 응답의 유형)
+        response.write(data); //응답의 바디 부분(표시할 내용)
+        response.end();
+    });    
+}
+
+function order(response, productId){
+    response.writeHead(200, {'Content-Type' : 'text/html'}); //응답의 헤드 부분(통신 상태, 응답의 유형)
+
+    mariadb.query("insert into orderlist values("+ productId +", '" + new Date().toLocaleDateString() + "');", function(err, rows){
+        console.log(rows);
+    });
+    
+    response.write('order page'); //응답의 바디 부분(표시할 내용)
+    response.end();
+}
 
 // function favicon(response){
 //     response.writeHead(404, {'Content-Type': 'image/x-icon' });
@@ -34,11 +53,15 @@ function login(response){
 // }
 // favicon.ico에 대한 request(웹 브라우저에서 자동으로 요구)가 없으면 router.js의 handle{}();메소드 부분에서 에러가 발생한다.
 // 이를 해결하기 위해 해당 케이스의 요청에 대해 404오류 코드로 응답하도록 한다.
+//handle['/favicon.ico'] = favicon;
 
 let handle = {}; // Dictionary > key:value 쌍으로 이루어진 변수
 handle['/'] = main;
-handle['/login'] = login;
-// handle['/junho'] = junho;
-//handle['/favicon.ico'] = favicon;
+handle['/order'] = order;
+
+/* image diirectory */
+handle['/img/diablo_4.jpg'] = diablo4;
+handle['/img/elden_ring.jpg'] = eldenRing;
+handle['/img/once_human.jpg'] = onceHuman;
 
 exports.handle = handle;
--- server.js
+++ server.js
@@ -4,8 +4,9 @@ let url = require('url');
 function start(route, handle){
     function onRequest(request, response){        
         let pathname = url.parse(request.url).pathname; // parse > 문자열을 캐치하는 메소드
-
-        route(pathname, handle, response);
+        let queryData = url.parse(request.url, true).query; 
+        
+        route(pathname, handle, response, queryData.productId);
     }
     
     http.createServer(onRequest).listen(8888);
